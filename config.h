da693c5b11456873932a979edaee014eca52b277
Francesco Minnocci <ad17fmin@uwcad.it>
2020-06-01 16:18:34 +0200

    switched to Noto Color Emoji

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14", "JoyPixels:pixelsize=14:antialias=true:autohint=true"  };
+static const char *fonts[]          = { "monospace:size=14", "Noto Color Emoji:pixelsize=14:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";

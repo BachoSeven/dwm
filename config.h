1e80d88c3380edbc6c7d2efd97f0d068e74c588b
Francesco Minnocci <ad17fmin@uwcad.it>
2020-06-22 17:18:47 +0200

    Updated config: comptoggle & camtoggle

--- config.h
+++ config.h
@@ -204,7 +204,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_z,		incrgaps,	{.i = +3 } },
 	/* { MODKEY|ShiftMask,		XK_z,		spawn,		SHCMD("") }, */
 	{ MODKEY,			XK_x,		incrgaps,	{.i = -3 } },
-	{ MODKEY|ShiftMask,		XK_x,		spawn,		SHCMD("xcompmgrtoggle") },
+	{ MODKEY|ShiftMask,		XK_x,		spawn,		SHCMD("comptoggle") },
 	{ MODKEY,			XK_c,		togglescratch,	{.ui = 1} },
 	/* { MODKEY|ShiftMask,		XK_c,		spawn,		SHCMD("") }, */
 	/* V is automatically bound above in STACKKEYS */
@@ -232,7 +232,7 @@ static Key keys[] = {
 
 	{ MODKEY,			XK_F1,		spawn,		SHCMD("mpc pause && mpc toggleoutput MPD && mpc toggleoutput MPD && mpc play") },
 	{ MODKEY,			XK_F2,		spawn,		SHCMD("connectHeadphones") },
-	// { MODKEY,			XK_F3,		spawn,		SHCMD("displayselect") },
+	{ MODKEY,			XK_F3,		spawn,		SHCMD("displayselect") },
 	{ MODKEY,			XK_F4,		spawn,		SHCMD("st -e pamix; kill -44 $(pidof dwmblocks)") },
 	// { MODKEY,			XK_F5,		spawn,		SHCMD(""), },
 	{ MODKEY,			XK_F6,		spawn,		SHCMD("torwrap") },
@@ -240,7 +240,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_F8,		spawn,		SHCMD("mailsync") },
 	{ MODKEY,			XK_F9,		spawn,		SHCMD("dmenumount") },
 	{ MODKEY,			XK_F10,		spawn,		SHCMD("dmenuumount") },
-	{ MODKEY,			XK_F11,		spawn,		SHCMD("mpv --no-cache --no-osc --no-input-default-bindings --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
+	{ MODKEY,			XK_F11,		spawn,		SHCMD("camtoggle") },
 	{ MODKEY,			XK_F12,		xrdb,		{.v = NULL } },
 	{ MODKEY,			XK_space,	zoom,		{0} },
 	{ MODKEY|ShiftMask,		XK_space,	togglefloating,	{0} },

07dfd3cd121297c4977041325a47759a9d7f2fff
Francesco Minnocci <ad17fmin@uwcad.it>
2020-08-06 10:50:11 +0200

    Minor Changes

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 30;       /* snap pixel */
+static const unsigned int snap      = 20;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */

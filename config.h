c2d03898f374b93aaae9e8e796cf634c500e4d9b
Francesco Minnocci <ad17fmin@uwcad.it>
2020-09-05 17:22:24 +0200

    Fix a typo.

--- config.h
+++ config.h
@@ -207,7 +207,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_s,		togglesticky,	{0} },
 	{ MODKEY|ShiftMask,		XK_s,		spawn,		SHCMD("killall screenkey >/dev/null || screenkey &") },
 	{ MODKEY,			XK_d,		spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,		XK_d,		spawn,		SHCMD("dcsg") },
+	{ MODKEY|ShiftMask,		XK_d,		spawn,		SHCMD("dscg") },
 	{ MODKEY,			XK_f,		togglefullscr,	{0} },
 	{ MODKEY|ShiftMask,		XK_f,		setlayout,	{.v = &layouts[8]} },
 	{ MODKEY,			XK_g,		shiftview,	{ .i = -1 } },

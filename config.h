3e91ec638c2ca1312aee1059ef61b4576ca95ab3
Francesco Minnocci <ad17fmin@uwcad.it>
2020-11-15 17:00:58 +0100

    Increase outer gaps

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static unsigned int borderpx  = 3;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static unsigned int gappih    = 18;       /* horiz inner gap between windows */
 static unsigned int gappiv    = 18;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
@@ -84,7 +84,7 @@ static Sp scratchpads[] = {
 
 static const char *const autostart[] = {
 	TERMINAL, "-c", "sysMon", "-e", "btm", NULL,
-	"sh", "-c", "'xdotool key super+F8'", NULL,
+	"sh", "-c", "\"xdotool key super+F8\"", NULL,
 	NULL /* terminate */
 };
 

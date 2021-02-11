2b5f3a43c14889583fea29bd2577c0212eea63a7
Francesco Minnocci <ad17fmin@uwcad.it>
2021-02-11 16:38:33 +0100

    Add a cute xmenu application launcher with icons and categories uwu

--- config.h
+++ config.h
@@ -375,6 +375,7 @@ static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 #ifndef __OpenBSD__
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkWinTitle,          0,              Button3,        spawn,          SHCMD("xm") },
 	{ ClkStatusText,        0,              Button1,        sigdwmblocks,   {.i = 1} },
 	{ ClkStatusText,        0,              Button2,        sigdwmblocks,   {.i = 2} },
 	{ ClkStatusText,        0,              Button3,        sigdwmblocks,   {.i = 3} },
@@ -395,4 +396,5 @@ static Button buttons[] = {
 	{ ClkTagBar,		0,		Button4,	shiftview,	{.i = -1} },
 	{ ClkTagBar,		0,		Button5,	shiftview,	{.i = 1} },
 	{ ClkRootWin,		0,		Button2,	togglebar,	{0} },
+	{ ClkRootWin,           0,              Button3,        spawn,          SHCMD("xm") },
 };
